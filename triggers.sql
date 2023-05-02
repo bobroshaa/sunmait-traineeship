b858565a9e34d995af0603138787a697bafd6822
bobroshaa <bobroshaa@mail.ru>
2023-05-02 19:16:51 +0300

    add creating one trigger instead of two

--- triggers.sql
+++ triggers.sql
@@ -1,37 +1,22 @@
-CREATE OR REPLACE FUNCTION log_order_status_on_update()
+CREATE OR REPLACE FUNCTION log_order_status_changes()
     RETURNS TRIGGER
-    LANGUAGE PLPGSQL
-    AS
+    LANGUAGE PLPGSQL AS
 $$
 BEGIN
-    IF NEW.current_status <> OLD.current_status THEN
+    IF (TG_OP = 'UPDATE' AND NEW.current_status <> OLD.current_status) THEN
         INSERT INTO order_history(order_id, status, date) 
         VALUES(OLD.id, NEW.current_status,  now());
+    END IF;
+    IF (TG_OP = 'INSERT') THEN
+        INSERT INTO order_history(order_id, status, date) 
+        VALUES(NEW.id, NEW.current_status,  now());
     END IF;
     RETURN NEW;
 END;
 $$;
 
-CREATE TRIGGER order_status_on_update
-  BEFORE UPDATE
-  ON customer_order
-  FOR EACH ROW
-  EXECUTE PROCEDURE log_order_status_on_update();
-
-CREATE OR REPLACE FUNCTION log_order_status_on_add()
-    RETURNS TRIGGER
-    LANGUAGE PLPGSQL
-    AS
-$$
-BEGIN
-    INSERT INTO order_history(order_id, status, date) 
-    VALUES(NEW.id, NEW.current_status,  now());
-    RETURN NEW;
-END;
-$$;
-
-CREATE TRIGGER order_status_on_add
-  AFTER INSERT
-  ON customer_order
-  FOR EACH ROW
-  EXECUTE PROCEDURE log_order_status_on_add();
+CREATE TRIGGER order_status_changes
+    AFTER INSERT OR UPDATE
+    ON customer_order
+    FOR EACH ROW
+    EXECUTE PROCEDURE log_order_status_on_update();
