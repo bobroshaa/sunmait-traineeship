d0b05ea918e2ae51e934406a1791a5303c15c604
bobroshaa <bobroshaa@mail.ru>
2023-05-27 15:41:35 +0300

    add repository for order item

---
+++ ClothingStore.Domain/Interfaces/IOrderItemRepository.cs
@@ -0,0 +1,11 @@
+﻿using ClothingStore.Domain.Entities;
+
+namespace ClothingStore.Domain.Interfaces;
+
+public interface IOrderItemRepository
+{
+    Task<IEnumerable<OrderProduct>> GetAllById(int id);
+    Task Add(OrderProduct orderItem);
+    Task Update(OrderProduct updatingOrderItem, OrderProduct orderItem);
+    Task Delete(OrderProduct orderItem);
+}
---
+++ ClothingStore.Infrastructure/Data/Repositories/OrderItemRepository.cs
@@ -0,0 +1,40 @@
+﻿using ClothingStore.Domain.Entities;
+using ClothingStore.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClothingStore.Infrastructure.Data.Repositories;
+
+public class OrderItemRepository : IOrderItemRepository
+{
+    private readonly Context _dbContext;
+
+    public OrderItemRepository(Context dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<IEnumerable<OrderProduct>> GetAllById(int id)
+    {
+        return await _dbContext.OrderProducts
+            .Where(op => op.OrderID == id)
+            .ToListAsync();
+    }
+    public async Task Add(OrderProduct orderItem)
+    {
+        await _dbContext.OrderProducts.AddAsync(orderItem);
+        await _dbContext.SaveChangesAsync();
+    }
+    
+    public async Task Update(OrderProduct updatingOrderItem, OrderProduct orderItem)
+    {
+        updatingOrderItem.Price = orderItem.Price;
+        updatingOrderItem.Quantity = orderItem.Quantity;
+        await _dbContext.SaveChangesAsync();
+    }
+    
+    public async Task Delete(OrderProduct orderItem)
+    {
+        // orderItem.IsActive = false;
+        // await _dbContext.SaveChangesAsync();
+    }
+}
