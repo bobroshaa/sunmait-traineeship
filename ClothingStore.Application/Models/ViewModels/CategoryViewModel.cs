5dfe5a195f12e5c073224c60541bbfc8d9c01c2e
bobroshaa <bobroshaa@mail.ru>
2023-05-28 23:42:35 +0300

    add models for category

---
+++ ClothingStore.Application/Models/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ClothingStore.Application.Models.ViewModels;
+
+public class CategoryViewModel
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public int? ParentCategoryID { get; set; }
+}
