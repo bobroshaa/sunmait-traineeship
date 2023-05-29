7e192a54fee6f2ff2229a897005972895c952680
bobroshaa <bobroshaa@mail.ru>
2023-05-29 22:13:33 +0300

    add section repository

---
+++ ClothingStore.Domain/Interfaces/ISectionRepository.cs
@@ -0,0 +1,9 @@
+﻿using ClothingStore.Domain.Entities;
+
+namespace ClothingStore.Domain.Interfaces;
+
+public interface ISectionRepository
+{
+    Task Add(Section section);
+    Task Update(Section updatingSection, string newName);
+}
---
+++ ClothingStore.Infrastructure/Data/Repositories/SectionRepository.cs
@@ -0,0 +1,26 @@
+﻿using ClothingStore.Domain.Entities;
+using ClothingStore.Domain.Interfaces;
+
+namespace ClothingStore.Infrastructure.Data.Repositories;
+
+public class SectionRepository : ISectionRepository
+{
+    private readonly Context _dbContext;
+
+    public SectionRepository(Context dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Add(Section section)
+    {
+        await _dbContext.Sections.AddAsync(section);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task Update(Section updatingSection, string newName)
+    {
+        updatingSection.Name = newName;
+        await _dbContext.SaveChangesAsync();
+    }
+}
