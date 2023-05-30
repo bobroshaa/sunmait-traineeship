27eb6fd28699097f1f4b645d5a8c07782dd74bb7
bobroshaa <bobroshaa@mail.ru>
2023-05-31 00:05:39 +0300

    add user input and view models

---
+++ ClothingStore.Application/Models/InputModels/UserInputModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClothingStore.Application.Models.InputModels;
+
+public class UserInputModel
+{
+    [StringLength(20, ErrorMessage = "The Phone field must not exceed 20 characters.")]
+    public string? Phone { get; set; }
+
+    [StringLength(100, ErrorMessage = "The Email field must not exceed 100 characters.")]
+    [Required(ErrorMessage = "The Email field is required.")]
+    public string Email { get; set; }
+
+    [StringLength(32, MinimumLength = 32, ErrorMessage = "The Password field must be equal 32 characters.")]
+    [Required(ErrorMessage = "The Password is required.")]
+    public string Password { get; set; }
+
+    [StringLength(50, ErrorMessage = "The FirstName field must be equal 32 characters.")]
+    [Required(ErrorMessage = "The FirstName is required.")]
+    public string FirstName { get; set; }
+
+    [StringLength(50, ErrorMessage = "The LastName field must be equal 32 characters.")]
+    [Required(ErrorMessage = "The LastName is required.")]
+    public string LastName { get; set; }
+}
---
+++ ClothingStore.Application/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ClothingStore.Domain.Entities;
+using ClothingStore.Domain.Enums;
+
+namespace ClothingStore.Application.Models.ViewModels;
+
+public class UserViewModel
+{
+    public int ID { get; set; }
+    public string? Phone { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public Role Role { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Address? Address { get; set; }
+}
