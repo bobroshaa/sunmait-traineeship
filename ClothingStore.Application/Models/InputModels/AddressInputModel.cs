72342cb19faa78597eef3713c1d35c604a86d2ad
bobroshaa <bobroshaa@mail.ru>
2023-05-31 00:36:13 +0300

    add input and view models for address

---
+++ ClothingStore.Application/Models/InputModels/AddressInputModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ClothingStore.Domain.Enums;
+
+namespace ClothingStore.Application.Models.InputModels;
+
+public class AddressInputModel
+{
+    [Required(ErrorMessage = "The UserID field is required.")]
+    public int UserID { get; set; }
+    
+    [Required(ErrorMessage = "The Country field is required.")]
+    public Country Country {get; set;}
+    
+    [Required(ErrorMessage = "The District field is required.")]
+    [StringLength(50, ErrorMessage = "The District field must not exceed 50 characters.")]
+    public string District { get; set; }
+    
+    [Required(ErrorMessage = "The City field is required.")]
+    [StringLength(50, ErrorMessage = "The City field must not exceed 50 characters.")]
+    public string City { get; set; }
+    
+    [Required(ErrorMessage = "The Postcode field is required.")]
+    [StringLength(10, ErrorMessage = "The Postcode field must not exceed 10 characters.")]
+    public string Postcode { get; set; }
+    
+    [Required(ErrorMessage = "The AddressLine1 field is required.")]
+    [StringLength(50, ErrorMessage = "The AddressLine1 field must not exceed 50 characters.")]
+    public string AddressLine1 { get; set; }
+    
+    [StringLength(20, ErrorMessage = "The AddressLine2 field must not exceed 20 characters.")]
+    public string? AddressLine2 { get; set; }
+}
