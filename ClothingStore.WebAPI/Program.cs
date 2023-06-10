cadf5e7b211c7add7c1a591e88d6bfd4a78e9ba9
bobroshaa <bobroshaa@mail.ru>
2023-06-11 00:39:02 +0300

    add authentication and configure in Program.cs

--- ClothingStore.WebAPI/Program.cs
+++ ClothingStore.WebAPI/Program.cs
@@ -1,11 +1,35 @@
 using System.Reflection;
+using System.Text;
 using ClothingStore.Application.Profiles;
 using ClothingStore.WebAPI;
 using ClothingStore.WebAPI.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+    .AddJwtBearer(jwt =>
+        {
+            var key = Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JwtConfig").GetValue<string>("Secret"));
+            jwt.SaveToken = true;
+            jwt.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                RequireExpirationTime = false,
+                ValidateLifetime = true
+            };
+        });
+
     builder.Services
         .AddApplicationDependencies()
         .AddInfrastructure();
@@ -48,6 +72,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
